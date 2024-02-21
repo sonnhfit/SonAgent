22d100c1675b67c52faef4120ab3ccddc6915250
son nguyen <son.nguyen2@encapital.io>
2024-02-21 20:15:16 +0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,103 +1,28 @@
-# pylint: disable=invalid-name, redefined-builtin
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../sonagent")
-))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'DeepAir'
-copyright = '2022, Son Nguyen Huu'
-author = 'Son Nguyen Huu'
-
+project = 'SonAgent'
+copyright = '2024, son nguyen'
+author = 'son nguyen'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "numpydoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.extlinks",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode",
-    "nbsphinx",
-    "recommonmark",
-    "sphinx_markdown_tables",
-    "sphinx_copybutton",
-    "sphinx_search.extension",
-]
-# Add any paths that contain templates here, relative to this directory.
+extensions = []
+
 templates_path = ['_templates']
+exclude_patterns = []
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_material'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'alabaster'
 html_static_path = ['_static']
-
-
-# Material theme options (see theme.conf for more information)
-html_theme_options = {
-
-    # Set the name of the project to appear in the navigation.
-    'nav_title': 'SonAgent',
-
-    # Set you GA account ID to enable tracking
-    'google_analytics_account': 'UA-XXXXX',
-
-    # Specify a base_url used to generate sitemap.xml. If not
-    # specified, then no sitemap will be built.
-    'base_url': 'https://sonagent.readthedocs.io',
-
-    # Set the color and the accent color
-    'color_primary': 'teal',
-    'color_accent': 'cyan',
-
-    # Set the repo location to get a badge with stats
-    'repo_url': 'https://github.com/sonnhfit/sonagent/',
-    'repo_name': 'sonagent',
-
-    # Visible levels of the global TOC; -1 means unlimited
-    'globaltoc_depth': 3,
-    # If False, expand all TOC entries
-    'globaltoc_collapse': False,
-    # If True, show hidden TOC entries
-    'globaltoc_includehidden': False,
-}
-
-
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
-}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,61 +1,14 @@
 .. SonAgent documentation master file, created by
-   sphinx-quickstart on Tue May 31 02:38:44 2022.
+   sphinx-quickstart on Wed Feb 21 20:14:19 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to SonAgent's documentation!
-===================================
-
-**SonAgent** (/diːp er/) is a Python library that have reliable implementations of Deep reinforcement learning algorithms.
-
-Simple way to start
--------------------
-Install SonAgent with pip
-
-.. code-block:: bash
-
-   pip install SonAgent
-
-start with gym env
-
-.. code-block:: python
-
-   import gym
-   from SonAgent.dqn import Rainbow
-
-   env = gym.make('LunarLander-v2')
-
-   rain = Rainbow(env=env, memory_size=10000, batch_size=32, target_update=256)
-
-   rain.train(timesteps=200000)
-
-   # test
-   state = env.reset()
-   done = False
-   score = 0
-
-   while not done:
-      action = rain.select_action(state, deterministic=True)
-      next_state, reward, done, info = env.step(action)
-
-      state = next_state
-      score += reward
-
-   print("score: ", score)
-
-
+====================================
 
 .. toctree::
-   :maxdepth: 1
-
-   install
-   tutorial
-   rlalgorithms
-   custom_technique
-   developer_guides
-   modules
-
-
+   :maxdepth: 2
+   :caption: Contents:
 
 
 
