07a1fb614ea76337e3e7aefebda07434db86c268
son nguyen <son.nguyen2@encapital.io>
2024-02-27 00:34:11 +0700

    updaet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,101 +1,103 @@
+# pylint: disable=invalid-name, redefined-builtin
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.abspath(
+    os.path.join(__file__, "../../sonagent")
+))
+
+
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SonAgent'
-copyright = '2024, son nguyen'
-author = 'son nguyen'
-release = '0.1'
+copyright = '2022, Son Nguyen Huu'
+author = 'Son Nguyen Huu'
+
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
-
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "numpydoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "recommonmark",
+    "sphinx_markdown_tables",
+    "sphinx_copybutton",
+    "sphinx_search.extension",
+]
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "pydata_sphinx_theme"
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_material'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 
-
-
+# Material theme options (see theme.conf for more information)
 html_theme_options = {
-    "external_links": [
-        {
-            "url": "#",
-            "name": "SonAgent",
-        }
-    ],
-    "header_links_before_dropdown": 4,
-    "icon_links": [
-        {
-            "name": "Twitter",
-            "url": "#",
-            "icon": "fa-brands fa-twitter",
-        },
-        {
-            "name": "GitHub",
-            "url": "https://github.com/sonnhfit/sonagent",
-            "icon": "fa-brands fa-github",
-        },
-        {
-            "name": "PyPI",
-            "url": "#",
-            "icon": "fa-custom fa-pypi",
-        }
-    ],
-    # alternative way to set twitter and github header icons
-    # "github_url": "https://github.com/pydata/pydata-sphinx-theme",
-    # "twitter_url": "https://twitter.com/PyData",
-    "logo": {
-        "text": "PyData Theme",
-        "image_dark": "_static/logo-dark.svg",
-    },
-    "use_edit_page_button": True,
-    "show_toc_level": 1,
-    "navbar_align": "left",  # [left, content, right] For testing that the navbar items align properly
-    # "show_nav_level": 2,
-    # "announcement": "https://raw.githubusercontent.com/pydata/pydata-sphinx-theme/main/docs/_templates/custom-template.html",
-    "show_version_warning_banner": True,
-    # "navbar_center": ["version-switcher", "navbar-nav"],
-    # "navbar_start": ["navbar-logo"],
-    # "navbar_end": ["theme-switcher", "navbar-icon-links"],
-    # "navbar_persistent": ["search-button"],
-    # "primary_sidebar_end": ["custom-template", "sidebar-ethical-ads"],
-    # "article_footer_items": ["test", "test"],
-    # "content_footer_items": ["test", "test"],
-    "footer_start": ["copyright"],
-    "footer_center": ["sphinx-version"],
-    "secondary_sidebar_items": {
-        "**/*": ["page-toc", "edit-this-page", "sourcelink"],
-        "examples/no-sidebar": [],
-    },
-    # "back_to_top_button": False,
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'SonAgent',
+
+    # Set you GA account ID to enable tracking
+    'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://sonagent.readthedocs.io',
+
+    # Set the color and the accent color
+    'color_primary': 'teal',
+    'color_accent': 'cyan',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/sonnhfit/SonAgent/',
+    'repo_name': 'SonAgent',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
 }
 
-favicons = [
-    # generic icons compatible with most browsers
-    "favicon-32x32.png",
-    "favicon-16x16.png",
-    {"rel": "shortcut icon", "sizes": "any", "href": "favicon.ico"},
-    # chrome specific
-    "android-chrome-192x192.png",
-    # apple icons
-    {"rel": "mask-icon", "color": "#459db9", "href": "safari-pinned-tab.svg"},
-    {"rel": "apple-touch-icon", "href": "apple-touch-icon.png"},
-    # msapplications
-    {"name": "msapplication-TileColor", "content": "#459db9"},
-    {"name": "theme-color", "content": "#ffffff"},
-    {"name": "msapplication-TileImage", "content": "mstile-150x150.png"},
-]
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,30 @@
+.. deepair documentation master file, created by
+   sphinx-quickstart on Tue May 31 02:38:44 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SonAgent's documentation!
+===================================
+
+Simple way to start
+-------------------
+
+
+.. toctree::
+   :maxdepth: 1
+
+   install
+   tutorial
+   developer_guides
+   modules
+
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
