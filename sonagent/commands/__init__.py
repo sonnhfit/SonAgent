e23f2d8413063778402fae5d43d626acbfcc3f11
son nguyen <son.nguyen2@encapital.io>
2024-04-07 13:14:24 +0700

    udpate

--- sonagent/commands/__init__.py
+++ sonagent/commands/__init__.py
@@ -1,3 +1,3 @@
 from sonagent.commands.arguments import Arguments  # noqa: F401
-from sonagent.commands.run_commands import (create_user_data_dir,  # noqa: F401
-                                            start_sonagent) # noqa: F401
+from sonagent.commands.run_commands import create_user_data_dir  # noqa: F401
+from sonagent.commands.run_commands import start_sonagent  # noqa: F401
