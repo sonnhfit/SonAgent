3bb614e4ab604e2e49d197f60008bdf1a668b02b
son nguyen <son.nguyen2@encapital.io>
2024-04-28 01:26:14 +0700

    updatE

--- sonagent/persistence/planning_models.py
+++ sonagent/persistence/planning_models.py
@@ -36,3 +36,90 @@ class Plan(ModelBase):
     def get_plan_by_ids(ids: List[int]) -> List["Plan"]:
         return Plan.session.scalars(
             select(Plan).filter(Plan.id.in_(ids)).order_by(Plan.created_at.asc())).all()
+
+
+class Artifact(ModelBase):
+    __tablename__ = "artifact"
+    __allow_unmapped__ = True
+    session: ClassVar[SessionType]
+
+    artifact_id: Mapped[str] = mapped_column(String, primary_key=True)
+    agent_created: Mapped[bool] = mapped_column(nullable=False, default=False)
+    relative_path: Mapped[str] = mapped_column(String, nullable=True)
+
+    @staticmethod
+    def get_all_artifacts() -> List["Artifact"]:
+        return Artifact.session.scalars(select(Artifact)).all()
+    
+    @staticmethod
+    def get_artifact_by_id(artifact_id: str) -> "Artifact":
+        return Artifact.session.scalars(
+            select(Artifact).filter(Artifact.artifact_id == artifact_id)).one()
+    
+    @staticmethod
+    def get_artifacts_by_ids(artifact_ids: List[str]) -> List["Artifact"]:
+        return Artifact.session.scalars(
+            select(Artifact).filter(Artifact.artifact_id.in_(artifact_ids))).all()
+    
+    @staticmethod
+    def get_artifacts_by_relative_path(relative_path: str) -> List["Artifact"]:
+        return Artifact.session.scalars(
+            select(Artifact).filter(Artifact.relative_path == relative_path)).all()
+
+
+class Task(ModelBase):
+    __tablename__ = "task"
+    __allow_unmapped__ = True
+    session: ClassVar[SessionType]
+
+    task_id = Mapped[str] = mapped_column(String, primary_key=True)
+    artifacts: Mapped[List[Artifact]] = mapped_column(Artifact, nullable=True)
+    steps: Mapped[List["Step"]] = mapped_column("Step", nullable=True)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[str] = mapped_column(
+        String, nullable=True, default=dt_now)
+
+    @staticmethod
+    def get_all_tasks() -> List["Task"]:
+        return Task.session.scalars(select(Task)).all()
+    
+    @staticmethod
+    def get_task_by_id(task_id: str) -> "Task":
+        return Task.session.scalars(
+            select(Task).filter(Task.task_id == task_id)).one()
+
+
+class Step(ModelBase):
+    __tablename__ = "step"
+    __allow_unmapped__ = True
+    session: ClassVar[SessionType]
+
+    task_id: Mapped[str] = mapped_column(String, primary_key=True)
+    step_id: Mapped[str] = mapped_column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=True)
+    status: Mapped[str] = mapped_column(String, nullable=True)
+    output: Mapped[str] = mapped_column(String, nullable=True)
+    additional_output: Mapped[str] = mapped_column(String, nullable=True)
+    artifacts: Mapped[List[Artifact]] = mapped_column(Artifact, nullable=True)
+    is_last: Mapped[bool] = mapped_column(nullable=False, default=False)
+    additional_properties: Mapped[str] = mapped_column(String, nullable=True)
+
+    @staticmethod
+    def get_all_steps() -> List["Step"]:
+        return Step.session.scalars(select(Step)).all()
+    
+    @staticmethod
+    def get_step_by_id(task_id: str, step_id: str) -> "Step":
+        return Step.session.scalars(
+            select(Step).filter(Step.task_id == task_id, Step.step_id == step_id)).one()
+    
+    @staticmethod
+    def get_steps_by_task_id(task_id: str) -> List["Step"]:
+        return Step.session.scalars(
+            select(Step).filter(Step.task_id == task_id)).all()
+    
+    @staticmethod
+    def get_step_by_task_id(task_id: str, step_id: str) -> "Step":
+        return Step.session.scalars(
+            select(Step).filter(
+                Step.task_id == task_id, Step.step_id == step_id)).one()
---
+++ sonagent/rpc/api_server/api_ap_v1.py
@@ -0,0 +1,147 @@
+import logging
+from typing import Annotated, List, Optional
+
+from fastapi import APIRouter, File, Form, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+
+from sonagent.persistence.planning_models import Artifact, Step, Task
+from sonagent.rpc.api_server.ap_models import (Pagination, StepRequestBody,
+                                               TaskListResponse,
+                                               TaskRequestBody,
+                                               TaskStepsListResponse)
+from sonagent.rpc.api_server.api_models import Ping
+
+logger = logging.getLogger(__name__)
+
+
+# Public API, requires no auth.
+router_public = APIRouter()
+# Private API, protected by authentication
+router = APIRouter()
+
+
+@router_public.post("/agent/tasks", response_model=Task, tags=["agent"])
+def create_agent_task(body: TaskRequestBody | None = None):
+    """
+    Create a new task.
+    """
+    task = Task()
+    task.save()
+
+    # trigger task handler
+
+    return JSONResponse(content=task, status_code=200)
+
+
+@router_public.get("/agent/tasks", response_model=Ping)
+async def list_agent_tasks_ids(page_size: int = 10, current_page: int = 1):
+    tasks = Task.get_all_tasks()
+    start_index = (current_page - 1) * page_size
+    end_index = start_index + page_size
+    return TaskListResponse(
+        tasks=tasks[start_index:end_index],
+        pagination=Pagination(
+            total_items=len(tasks),
+            total_pages=len(tasks) // page_size,
+            current_page=current_page,
+            page_size=page_size,
+        ),
+    )
+
+
+@router_public.get("/agent/tasks/{task_id}", tags=["agent"])
+async def get_agent_task(task_id: str):
+    """
+    Get details about a specified agent task.
+    """
+    return Task.get_task_by_id(task_id)
+
+
+@router_public.get(
+    "/agent/tasks/{task_id}/steps",
+    response_model=TaskStepsListResponse,
+    tags=["agent"],
+)
+async def list_agent_task_steps(
+    task_id: str, page_size: int = 10, current_page: int = 1
+):
+    """
+    List all steps for the specified task.
+    """
+    task = Task.get_task_by_id(task_id)
+    start_index = (current_page - 1) * page_size
+    end_index = start_index + page_size
+    return TaskStepsListResponse(
+        steps=task.steps[start_index:end_index],
+        pagination=Pagination(
+            total_items=len(task.steps),
+            total_pages=len(task.steps) // page_size,
+            current_page=current_page,
+            page_size=page_size,
+        ),
+    )
+
+
+@router_public.post(
+    "/agent/tasks/{task_id}/steps",
+    response_model=Step,
+    tags=["agent"],
+)
+async def execute_agent_task_step(
+    task_id: str,
+    body: StepRequestBody | None = None,
+) -> Step:
+    pass
+
+
+@router_public.get(
+    "/agent/tasks/{task_id}/steps/{step_id}",
+    response_model=Step,
+    tags=["agent"],
+)
+async def get_agent_task_step(task_id: str, step_id: str):
+    return Step.get_step_by_task_id(task_id=task_id, step_id=step_id)
+
+
+@router_public.get(
+    "/agent/tasks/{task_id}/artifacts",
+    response_model=List[Artifact],
+    tags=["agent"],
+)
+async def list_agent_task_artifacts(task_id: str) -> List[Artifact]:
+    """
+    List all artifacts for the specified task.
+    """
+    pass
+
+
+@router_public.post(
+    "/agent/tasks/{task_id}/artifacts",
+    response_model=Artifact,
+    tags=["agent"],
+)
+async def upload_agent_task_artifacts(
+    task_id: str,
+    file: Annotated[UploadFile, File()],
+    relative_path: Annotated[Optional[str], Form()] = None,
+) -> Artifact:
+    """
+    Upload an artifact for the specified task.
+    """
+    pass
+
+
+@router_public.get(
+    "/agent/tasks/{task_id}/artifacts/{artifact_id}",
+    tags=["agent"],
+)
+async def download_agent_task_artifacts(task_id: str, artifact_id: str) -> FileResponse:
+    """
+    Download the specified artifact.
+    """
+    artifact = object
+    path = ""
+    return FileResponse(
+        path=path, media_type="application/octet-stream",
+        filename=artifact.file_name
+    )
